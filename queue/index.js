18ace6e25f7ee3475efca10ba2876b1382233395
Alexander Demidov <alex.s.demidov@gmail.com>
2016-12-14 16:42:20 +0700

    add rethinkdb queue type

--- queue/index.js
+++ queue/index.js
@@ -5,8 +5,13 @@ const config = require("../components").config;
 // Initialize storage
 let queue;
 switch (config.queue) {
+case "rethinkdb":
+    // use storage to produce events to RethinkDB
+    break;
 case "kafka":
     queue = require("./kafka");
     break;
+default:
+    throw new Error("Unsupported queue type");
 }
 module.exports = queue;
