5a5107f3f7dcd9ee2c9ef8c5e0a41989d55082a4
Alexander Demidov <alex.s.demidov@gmail.com>
2016-12-15 15:09:03 +0700

    use console.error

--- components.js
+++ components.js
@@ -69,7 +69,7 @@ function init() {
             .then(() => storage.checkDB())
             .then(() => module.exports);
     }).catch(err => {
-        console.log(err);
+        console.error(err);
         process.exit(1);
     });
 }
